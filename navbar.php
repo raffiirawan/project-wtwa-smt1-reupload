0bb902b63a0e888006d44434a219481c609ed163
Raffi Irawan <raffi.muafa22@gmail.com>
2024-12-26 11:47:05 +0700

    update navbar

--- navbar.php
+++ navbar.php
@@ -13,17 +13,20 @@ $genre_result = $conn->query($genre_query);
             <h1>NEDLIX</h1>
         </a>
         <div class="nav-list">
+            <a href="/project/index.php">All Movies</a>
+            <a href="/project/genre.php">Genre</a>
             <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
                 <!-- Jika sudah login -->
-                <a href="/project/index.php">All Movies</a>
-                <!-- <a href="/project/genre.php">Genre</a> -->
                 <a href="/project/admin/dashboard.php">Dashboard</a>
                 <a href="/project/admin/register.php">Add User</a>
                 <a href="/project/admin/logout.php">Logout</a>
             <?php else: ?>
                 <!-- Jika belum login -->
-                <a href="/project/index.php">All Movies</a>
-                <!-- <a href="/project/genre.php">Genre</a> -->
+                 <form action="">
+                    <!-- <label for="movieSearch">Search...</label> -->
+                    <input type="text" name="movieSearch" placeholder="Search...">
+                 </form>
+                <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
                 <a href="/project/admin/login.php">Login</a>
             <?php endif; ?>
         </div>
